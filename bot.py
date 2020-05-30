86888d2d68cc89f947b32d264a7a2557946734ad
Kiryha <kiryhaaa13@gmail.com>
2020-05-30 14:36:15 +0300

    Add structure to bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+
+
+class BotHandler:
+
+    def __init__(self):
+        pass
+
+    def get_updates(self):
+        pass
+
+    def send_message(self):
+        pass
+
+    def read_token(self):
+        pass
+
+    def get_last_update(self):
+        pass
+
+
+text_bot = BotHandler()
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
